73800f5483c79a6b1a746ea365622f9c93090cf7
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-12-02 15:29:54 +0200

    Load Next Page test

--- src/actions/loadPage.test.js
+++ src/actions/loadPage.test.js
@@ -202,4 +202,66 @@ describe('loadPage', () => {
       expect(mockGetOne.mock.calls).toEqual([[1], [2]])
     })
   })
+
+  test('Load Next Page', async () => {
+    setActivePinia(createPinia())
+    const mockConnector = vi.fn().mockImplementation(async (params, query) => {
+      const items = [{ _id: 1, name: 'test1' }, { _id: 2, name: 'test2' }].slice(query.skip, query.skip + query.limit)
+      return {
+        items,
+        count: 2
+      }
+    })
+    const mockOnError = vi.fn()
+
+    const useStore = defineStore('loadTestList', {
+      state: pagedState,
+      actions: {
+        loadPage: createLoadPage(mockConnector, mockOnError)
+      }
+    })
+
+    const store = useStore()
+    store.params = { param1: 'testparam', param2: 'testparam2' }
+    store.filter = { name: 'testname' }
+    store.select = { name: 1 }
+    store.sort = { name: -1 }
+    store.itemsPerPage = 1
+    store.items = []
+    store.count = 2
+
+    const resultPromise = store.loadPage(1)
+
+    const result = await resultPromise
+
+    expect(result).toEqual({
+      items: [
+        { _id: 1, name: 'test1' }
+      ],
+      count: 2
+    })
+    expect(store.status).toBe('ready')
+    expect(store.items.length).toBe(1)
+    expect(store.count).toBe(2)
+    expect(store.pageNum).toBe(1)
+    expect(store.numOfPages).toBe(2)
+    expect(store.items[0]._id).toBe(1)
+    expect(store.items[0].status).toBe('ready')
+    expect(store.items[0].data.name).toBe('test1')
+
+    const resultPromiseNext = store.loadPage(2)
+
+    const resultNext = await resultPromiseNext
+
+    expect(resultNext).toEqual({
+      items: [
+        { _id: 2, name: 'test2' }
+      ],
+      count: 2
+    })
+
+    expect(store.items[0]._id).toBe(2)
+    expect(store.items[0].status).toBe('ready')
+    expect(store.items[0].data.name).toBe('test2')
+  })
 })
